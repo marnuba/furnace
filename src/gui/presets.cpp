46a55ba4e38bdc3973c585c96aba1972edabebaa
Marko Nußbaum <marko@marnuba.com>
2023-07-26 11:23:31 +0200

    add presets for Foenix F256x

--- src/gui/presets.cpp
+++ src/gui/presets.cpp
@@ -1120,6 +1120,25 @@ void FurnaceGUI::initSystemPresets() {
     }
   );
   ENTRY(
+    "Foenix F256K ", {
+      CH(DIV_SYSTEM_C64_6581, 1.0f, 0, "clockSel=1"),
+      CH(DIV_SYSTEM_C64_6581, 1.0f, 0, "clockSel=1"),
+      CH(DIV_SYSTEM_SMS, 1.0f, 0, "chipType=1"),
+      CH(DIV_SYSTEM_SMS, 1.0f, 0, "chipType=1"),
+      CH(DIV_SYSTEM_OPL3, 1.0f, 0, "")
+    },
+    "tickRate=60"
+  );
+  ENTRY(
+    "Foenix F256Jr", {
+      CH(DIV_SYSTEM_C64_6581, 1.0f, 0, "clockSel=1"),
+      CH(DIV_SYSTEM_C64_6581, 1.0f, 0, "clockSel=1"),
+      CH(DIV_SYSTEM_SMS, 1.0f, 0, "chipType=1"),
+      CH(DIV_SYSTEM_SMS, 1.0f, 0, "chipType=1")
+    },
+    "tickRate=60"
+  );
+  ENTRY(
     "Commander X16 (VERA only)", {
       CH(DIV_SYSTEM_VERA, 1.0f, 0, "")
     }
